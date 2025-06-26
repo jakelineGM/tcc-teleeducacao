5d005b4668f65a8fef7d39e4cc16bffb8ddf7a22
[jakeline-gm] <[jakeline.mesquita@icomp.ufam.edu.br]>
2025-06-25 23:26:14 -0400

    feat(ui): implementa componentes do site - navbar e menu lateral

--- frontend/prototype/navbar.js
+++ frontend/prototype/navbar.js
@@ -1,10 +1,32 @@
-// navbar.js
-/*function personalizarNavbar() {
+// Utilizado pela navbar-publico
+function personalizarNavbar() {
   const usuario = JSON.parse(localStorage.getItem('usuarioLogado'));
-  if (usuario) {
-    const primeiroNome = usuario.nome.split(" ")[0];
-    const avatar = `<span>👤 Olá, ${primeiroNome}</span>`;
-    const entrarBtn = document.querySelector('.navbar-principal .entrar');
-    if (entrarBtn) entrarBtn.outerHTML = avatar;
+  const loginBtn = document.getElementById('login-btn');
+  
+  if (usuario && loginBtn) {
+    const navActions = loginBtn.parentElement;
+    loginBtn.remove();
+
+    const painelBtn = document.createElement('a');
+    painelBtn.href = "dashboard-publico.html";
+    painelBtn.className = "botao";
+    painelBtn.innerText = "Painel";
+
+    const sairBtn = document.createElement('a');
+    sairBtn.href = "#";
+    sairBtn.className = "botao sair navbat";
+    sairBtn.innerText = "Sair";
+    sairBtn.onclick = logout;
+
+    navActions.appendChild(painelBtn);
+    navActions.appendChild(sairBtn);
   }
-}*/
+}
+
+// Utilizado pelas navbars e menus laterais
+function logout() {
+  localStorage.removeItem('usuarioLogado');
+  window.location.href = "index.html";
+}
+
+window.onload = personalizarNavbar;
